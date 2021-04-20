868e165169bc12f4dc7888be0f6a2718478070af
Xiaotian-WANG <wangxt96@126.com>
2021-04-20 11:13:23 +0800

    1

--- test.py
+++ test.py
@@ -3,14 +3,14 @@ from zhon import hanzi
 import string
 from tf_idf import tf_idf, tf, idf, df
 
-myclasses_dict = {"航空器系统/部件失效":0,
-             "航空器设计制造缺陷":1,
-             "机务人员致灾":2,
-             "机组人员致灾":3,
-             "零件生产质量问题":4,
-             "运营管理问题":5,
-             "程序规章手册缺陷":6,
-             "安检空管维修资质等其它":7}
+myclasses_dict = {"航空器系统/部件失效": 0,
+             "航空器设计制造缺陷": 1,
+             "机务人员致灾": 2,
+             "机组人员致灾": 3,
+             "零件生产质量问题": 4,
+             "运营管理问题": 5,
+             "程序规章手册缺陷": 6,
+             "安检空管维修资质等其它": 7}
 
 def get_frequency(X):
     word_frequency = {}
